cae2803487879fc40f8148114bb3556d3e28e4ee
AdonisJeronimo <adonisfjeronimo@gmail.com>
2023-04-10 16:58:18 -0300

    Aula 12 - OPERADOR TERNARIO - BOOLEAN

---
+++ Section-2/12-operador-ternario/script.js
@@ -0,0 +1,13 @@
+/*
+O operador ternario faz parte do BOOLEANS, ele faz um compatarivo em apenas uma linha de código utilizando o ?  e o :.
+EXEMPLO:
+
+console.log(true? 1:2)
+
+No código acima estamos dizendo que, se a condição for True(?) ira retornar 1 senão(:) ira retornar 2
+
+NÃO É MUITO UTILIZADO PORQUE DEPENDENDO DE COMO É PODE DEIXAR O CÓDIGO COMPLICADO DE LER, principalmente se sua equipe
+não esta acosmutada com este tipo o que dificulta sua interpretação.
+*/
+
+console.log(false?"1":"2");
