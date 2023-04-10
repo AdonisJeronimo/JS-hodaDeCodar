3c932361a14c9db6aae3ed7cdd8529d510e1ce6f
AdonisJeronimo <adonisfjeronimo@gmail.com>
2023-04-10 13:40:00 -0300

    Curso JS Hora de Codar

---
+++ Section-2/2-tipos-de-dados/script.js
@@ -0,0 +1,19 @@
+/* O que são tipos de dados?
+
+Os tipos de dados existentes no JavaScrip são: */
+
+// Number (Aritimético,Special Numbers);
+
+a = 10,
+
+// Strings (Text);
+
+b = "Dez"
+
+// Boolean (TRUE or FALSE);
+
+a === 10
+
+//Null;
+//Undefined;
+//Symbol;
---
+++ Section-2/3-number/script.js
@@ -0,0 +1,11 @@
+/* Numbers - é um tipo de dado númerico que é escrito sem nenhum tipo de aspas.
+
+-> Para verificarmos o tipo de dado, podemos utilizar o typeof.
+
+OBS: Para numeros sempre iremos utilziar PONTO e NÃO VIRGULA.
+
+*/
+
+console.log(typeof 13);
+console.log(typeof 1.8);
+console.log(typeof -5);
