40f3e869bc5a12a3f839c613d4edd4bd6e21cccd
AdonisJeronimo <adonisfjeronimo@gmail.com>
2023-04-10 17:04:55 -0300

    Aula 13 de Empty Value - NULL and UNDEFINED

---
+++ Section-2/13-empty-value/script.js
@@ -0,0 +1,14 @@
+/*
+Para o Empty Value existem duas palavras reservadas da linguagem para estes casos, que são:
+
+    UNDEFINED
+    NULL
+
+Sempre que depararmos com estar palavras, o JS basicamente quer nos dizer que os valores não existem ou nulos;
+
+OBS: EXISTEM PALAVRAS RESERVADAS DO JS QUE NÃO PODEM SER USADAS PARA CRIARMOS VARIAVEIS
+*/
+
+console.log(null);
+console.log(undefined);
+
