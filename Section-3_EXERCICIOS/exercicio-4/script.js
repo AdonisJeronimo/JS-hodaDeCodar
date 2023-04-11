475daa8af1454c329f744a50561e38b5d0ec1e3a
AdonisJeronimo <adonisfjeronimo@gmail.com>
2023-04-11 07:46:02 -0300

    Exercicio 4

---
+++ Section-3_EXERCICIOS/exercicio-4/script.js
@@ -0,0 +1,16 @@
+a = 10;
+b = 20;
+
+//AND
+
+console.log(a < b && b == a)
+
+//OR
+
+console.log( a > b || b > a)
+
+//NOT
+
+console.log(!(a != b || b > a)) // false (por estarmos negando)
+
+// console.log(a != b || b > a) - true (sem o NOT)
